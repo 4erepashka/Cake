ae1207e697e30f31d92d0cc11f5ddb4728d19429
4erepashka <92363518+4erepashka@users.noreply.github.com>
2024-01-18 22:49:22 +0300

    Update eye_animation.js

--- js/eye_animation.js
+++ js/eye_animation.js
@@ -3,7 +3,7 @@ const animation = lottie.loadAnimation({
     renderer: 'svg', 
     loop: true, 
     autoplay: true, 
-    path: './animation/anima.json' 
+    path: '../Cake/animation/anima.json' 
   });
 
   animation.play();
