9d784ee8df196c6fbeab7b70f81d51716045386c
4erepashka <92363518+4erepashka@users.noreply.github.com>
2024-01-18 22:48:57 +0300

    Update mousse_eye.js

--- js/mousse_eye.js
+++ js/mousse_eye.js
@@ -2,7 +2,7 @@ const animation3 = lottie.loadAnimation({
     container: document.getElementById('lottie-animation3'), 
     loop: true, 
     autoplay: true, 
-    path: './animation/anima.json' 
+    path: '../Cake/animation/anima.json' 
   });
 
   animation3.play();
